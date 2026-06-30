ff80480776f68132edc75155cef8adb294826950
Sujal Sinha <me@sujalsinha.com>
2026-06-30 20:14:46 +0530

    add test: shows a link to the github homepage for this repository

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,35 @@
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+import RepositoriesListItem from "./RepositoriesListItem";
+
+function renderComponent() {
+	const repository = {
+		full_name: "facebook/react",
+		language: "JavaScript",
+		description: "A JS library",
+		owner: {
+			login: "facebook",
+		},
+		name: "react",
+		html_url: "https://github.com/facebook/react",
+	};
+
+	render(
+		<MemoryRouter>
+			<RepositoriesListItem repository={repository} />
+		</MemoryRouter>,
+	);
+
+	return { repository };
+}
+
+test("shows a link to the github homepage for this repository", async () => {
+	const { repository } = renderComponent();
+
+	await screen.findByRole("img", { name: "JavaScript" });
+
+	const link = screen.getByRole("link", { name: /github repository/ });
+	expect(link).toHaveAttribute("href", repository.html_url);
+});
+
