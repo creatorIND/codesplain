01f1d987e5e80a078a66d6028e7f041ce478157c
Sujal Sinha <me@sujalsinha.com>
2026-07-02 20:04:50 +0530

    bug fix: show all six languages' popular repositories

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -11,6 +11,10 @@ function HomeRoute() {
 	);
 	const { data: rustRepos } = useRepositories("stars:>10000 language:rust");
 	const { data: goRepos } = useRepositories("stars:>10000 language:go");
+	const { data: pythonRepos } = useRepositories(
+		"stars:>10000 language:python",
+	);
+	const { data: javaRepos } = useRepositories("stars:>10000 language:java");
 
 	return (
 		<div>
@@ -31,6 +35,14 @@ function HomeRoute() {
 				<RepositoriesTable
 					label="Most Popular Go"
 					repositories={goRepos}
+				/>
+				<RepositoriesTable
+					label="Most Popular Python"
+					repositories={pythonRepos}
+				/>
+				<RepositoriesTable
+					label="Most Popular Java"
+					repositories={javaRepos}
 				/>
 			</div>
 		</div>
